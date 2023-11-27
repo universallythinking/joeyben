f8124e6f7a91daf67ea4efe03860f68e5237df71
Benjamin Barnett <benjaminbarnett@Benjamins-MacBook-Air.local>
2023-11-27 11:25:42 -0500

    TEST

--- www/js/index.js
+++ www/js/index.js
@@ -28,12 +28,27 @@ var search = function() {
 	    dataType: "json",
 	    data: obj,
 	    success: function(data) {
-	    	var dataString = "First result: " + JSON.stringify(data.body[0].title);
-	    	$(["#firstResponse", "#responseData"]).empty(); //empty out the last results
-	    	$("#firstResponse").text(dataString); //add our response data to the page
-	    	$("#responseData").text(JSON.stringify(data));
+	    	var dataString;
+	    	$("#responseData").empty(); //empty out the last results
+	    	$.each(data.body, function(k, v) {
+				dataString = "<p class='record' data='" + v.videoId + "' onclick='playVideo(" + k + ");'>Result " + k + " Title: " + v.title + "</p>";
+				$("#responseData").append(dataString);  //add our nth response data to the responseData DIV
+	    	});
 	    }, error: function() {
 	    }, complete: function() {
 	    }
 	});
 }
+
+var videoUrl;
+
+window.playVideo = function(n) {
+	videoUrl = "https://www.youtube.com/embed/" + $(".record").eq(n).attr("data") + "?rel=0&autoplay=1&fs=1&playsinline=1&enablejsapi=1&mute=0&showinfo=0&controls=0";
+	$("#currentVideo").attr("src", videoUrl);
+}
+
+$("#searchBox").keydown(function(e){
+    if (e.keyCode == 13) {
+    	search();
+    }
+});
